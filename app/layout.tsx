c363b5619cd2cfafc7aa1aaca250c39819410cae
iammurali <muralismail4u@gmail.com>
2024-04-19 19:23:15 +0530

    add whatsapp link

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         {children}
         <footer>
           <div className="text-center text-sm h-20 bg-[#54331C] flex items-center justify-center text-white">
-            Whatsapp +91 8667473511 to order Now
+            Whatsapp <a className="underline px-2" href="https://wa.me/918667473511?text=Hello%2C%20I%20want%20to%20order%20from%20Anupakam">+91 8667473511</a> to order Now
           </div>
         </footer>
       </body>
